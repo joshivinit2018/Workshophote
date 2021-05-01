7ce37c785ca78356b5564776323dd29914b9f7f3
joshivinit2018 <joshivinit2018@gmailcom>
2021-05-01 13:03:37 +0530

    [vinit]: add find cheapest hotel

--- src/test/java/com/bridgelabz/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -14,4 +14,20 @@ public class HotelReservationTest {
         List<Hotel> hotelList = hotelReservationService.getHotels();
         Assertions.assertTrue(hotelList.contains(hotel));
     }
+
+    @Test
+   public void given3Hotels_whenInvokeFindCheapestHotel_shouldBeAbleToReturnCheapestHotel() {
+   HotelReservationService hotelReservationService = new HotelReservationService();
+   Hotel Lakewood = new Hotel("Lakewood",110);
+   Hotel Bridgewood = new Hotel("Bridgewood",160);
+   Hotel Ridgewood = new Hotel("Ridgewood",220);
+   hotelReservationService.addHotel(Lakewood);
+   hotelReservationService.addHotel(Bridgewood);
+   hotelReservationService.addHotel(Ridgewood);
+   int countDays = hotelReservationService.countDays("2020-09-10","2020-09-12");
+   System.out.println("Total count days : " +countDays);
+   Hotel result = hotelReservationService.findCheapestHotel();
+   Assertions.assertTrue(hotelReservationService.hotelList.contains(result));
+
+    }
 }
